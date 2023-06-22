92b8f3d137c4d6b542ed45f429d5bd025ec36d26
McDev <mcdev8@gmail.com>
2023-06-22 18:13:13 -0500

    added initial java code

---
+++ src/main/java/com/timetravelerboutique/timetravelerboutique/TimetravelerboutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.timetravelerboutique.timetravelerboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetravelerboutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetravelerboutiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timetravelerboutique/timetravelerboutique/TimetravelerboutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timetravelerboutique.timetravelerboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetravelerboutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
