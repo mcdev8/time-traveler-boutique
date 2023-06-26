90b524eb3434d9c63ab87d18a0d731fd7beb1982
McDev <mcdev8@gmail.com>
2023-06-26 18:22:21 -0500

    added a comment

--- src/main/java/com/timetravelerboutique/timetravelerboutique/TimetravelerboutiqueApplication.java
+++ src/main/java/com/timetravelerboutique/timetravelerboutique/TimetravelerboutiqueApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimetravelerboutiqueApplication {
-
+// McDev was here!
+// McDev was here!
 	public static void main(String[] args) {
 		SpringApplication.run(TimetravelerboutiqueApplication.class, args);
 	}
